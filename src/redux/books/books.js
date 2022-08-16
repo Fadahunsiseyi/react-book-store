f45222f8d2d5a6653afff4e6ee540ae78e5e8727
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-16 09:30:45 +0100

    Add the reducer for the addbook and removebook actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -8,6 +8,20 @@ export const addBook = (book) => ({
 });
 
 export const removeBook = (id) => ({
-    type: "REMOVE_BOOK",
-    payload: id,
-})
+  type: "REMOVE_BOOK",
+  payload: id,
+});
+
+const initialState = [];
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [...state, action.payload];
+    case "REMOVE_BOOK":
+      return state.filter((book) => book.id !== action.payload);
+
+    default:
+      return state;
+  }
+};
