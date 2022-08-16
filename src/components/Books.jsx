3384801af544922c4e90849580cf93186334e873
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-16 22:33:41 +0100

    Connect react redux to the application

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -4,20 +4,7 @@ import AddForm from './AddForm';
 import Book from './Book';
 
 const Books = () => {
-  const book = useSelector((state) => console.log(state.booksReducer));
-  console.log(book);
-  const books = [
-    {
-      id: 1,
-      title: 'Things fall apart',
-      author: 'Chinua Achebe',
-    },
-    {
-      id: 2,
-      title: 'Silver Spoon',
-      author: 'Chinua Achebe',
-    },
-  ];
+  const books = useSelector((state) => state.booksReducer);
   return (
     <>
       {books.map((book) => (
