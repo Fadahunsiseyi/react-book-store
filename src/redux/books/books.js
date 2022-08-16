ad547f209b41f062bbcfb0591d35200d791cb558
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-16 09:59:50 +0100

    Add reducer for the categories

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -16,9 +16,9 @@ const initialState = [];
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_BOOK':
+    case ADD_BOOK:
       return [...state, action.payload];
-    case 'REMOVE_BOOK':
+    case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
 
     default:
