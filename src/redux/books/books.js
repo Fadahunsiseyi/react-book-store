5d4f8a94907ab762c956551c86ec2ea34f0f5a20
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-16 09:10:43 +0100

    Add the addbook action creator

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,3 +1,8 @@
-const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const ADD_BOOK = "bookstore/books/ADD_BOOK";
 
-const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const REMOVE_BOOK = "bookstore/books/REMOVE_BOOK";
+
+export const addBook = (book) => ({
+  type: "ADD_BOOK",
+  payload: book,
+});
