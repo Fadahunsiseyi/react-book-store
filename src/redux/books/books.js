de0eeeddbe8637b7984a82777e6236143a34b312
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-18 10:04:49 +0100

    Add fetch books actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/ADD_BOOK';
 
-const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const FETCH_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 const BOOK_API = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+
+const fetchBooks = async (dispatch) => {
+    const result = await axios.get(BOOK_API);
+    dispatch({ type: FETCH_BOOK, payload: result.data });
+}
 
 export const addBook = (book) => ({
   type: ADD_BOOK,
@@ -20,11 +26,8 @@ const initialState = [];
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ADD_BOOK:
-      return [...state, action.payload];
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
-
+    case FETCH_BOOK:
+      return action.payload;
     default:
       return state;
   }
