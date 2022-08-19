b0fd795ff9815bdbecb9e21a1f534c9ba1284033
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-19 03:20:02 +0100

    Fetch data from the api using the asyncthunk method

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,16 +1,23 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const FETCH_BOOK = 'bookstore/books/REMOVE_BOOK';
+const FETCH_BOOK = 'bookstore/books/FETCH_BOOK';
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 const BOOK_API = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/mfsMj8Rjeqs9e9h9wYCP/books';
 
-const initialState = {};
+const initialState = {
+  booksLists: [],
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_BOOK:
-      return action.payload;
+    case `${FETCH_BOOK}/fulfilled`:
+      return {
+        ...state,
+        booksLists: [...action.payload],
+      };
     default:
       return state;
   }
